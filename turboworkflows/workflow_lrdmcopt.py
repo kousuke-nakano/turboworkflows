de9041ad77bf2ac7455e87e35129cf922714cf16
kousuke-nakano <37653569+kousuke-nakano@users.noreply.github.com>
2025-12-02 16:38:49 +0900

    Introduced num_opt_param option to workflow_lrdmcopt

--- turboworkflows/workflow_lrdmcopt.py
+++ turboworkflows/workflow_lrdmcopt.py
@@ -45,6 +45,7 @@ class LRDMCopt_workflow(Workflow):
         lrdmcopt_optimizer: str = "sr",
         lrdmcopt_learning_rate: float = 0.002,
         lrdmcopt_regularization: float = 0.001,
+        lrdmcopt_num_opt_param: int = 0,
         lrdmcopt_alat: float = -0.20,
         lrdmcopt_time_branching: float = 0.10,
         lrdmcopt_trial_etry: float = 0.0,
@@ -84,6 +85,7 @@ class LRDMCopt_workflow(Workflow):
         self.lrdmcopt_optimizer = lrdmcopt_optimizer
         self.lrdmcopt_learning_rate = lrdmcopt_learning_rate
         self.lrdmcopt_regularization = lrdmcopt_regularization
+        self.lrdmcopt_num_opt_param = lrdmcopt_num_opt_param
         self.lrdmcopt_alat = lrdmcopt_alat
         self.lrdmcopt_time_branching = lrdmcopt_time_branching
         self.lrdmcopt_trial_etry = lrdmcopt_trial_etry
@@ -260,6 +262,7 @@ class LRDMCopt_workflow(Workflow):
                         optimizer=self.lrdmcopt_optimizer,
                         learning_rate=self.lrdmcopt_learning_rate,
                         regularization=self.lrdmcopt_regularization,
+                        num_opt_param=self.lrdmcopt_num_opt_param,
                         alat=self.lrdmcopt_alat,
                         time_branching=self.lrdmcopt_time_branching,
                         etry=self.lrdmcopt_trial_etry,
